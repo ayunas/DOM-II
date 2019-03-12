9ef72239e7b30c038819fcf6f682e635de3b1e2a
ayunas <ayunas@gmail.com>
2019-03-12 16:20:54 -0500

    used javascript to color the text of the nav elements upon mouseover and mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const title = document.querySelector('.logo-heading');
+
+title.addEventListener('mouseover',function() {
+    title.style.color = '#FDBA58';    //@bus-yellow
+})
+
+title.addEventListener('mouseout',function() {
+    title.style.color = 'black';
+})
+
+const a = document.querySelectorAll('.nav-link');
+
+a.forEach(atag => atag.addEventListener('mouseover', function() {
+    atag.style.color = '#FDBA58';
+}));
+
+a.forEach(atag => atag.addEventListener('mouseout', function() {
+    atag.style.color = 'black';
+}));
+
+
+
+
+
